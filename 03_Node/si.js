44018b4bc8395ee5e557dcd625db2ee3bea22000
Kaseem <chavarria.martinez.victor101@gmail.com>
2025-03-23 23:07:11 -0600

    03_Node/si.js code fixed
    no existia square en el contexto

--- 03_Node/si.js
+++ 03_Node/si.js
@@ -9,7 +9,7 @@ class Rectangle {
     }
     // Method
     calcArea() {
-      return this.height * this.width;
+        return this.height * this.width;
     }
     *getSides() {
         yield this.height;
@@ -17,31 +17,28 @@ class Rectangle {
         yield this.height;
         yield this.width;
     }
-    get perimeter()
-    {
+    get perimeter() {
         return this.calcPeri();
     }
-    get pera2()
-    {
+    get pera2() {
         return this.calcPeri2();
     }
-    calcPeri()
-    {
-        let peri = square.getSides();
+    calcPeri() {
+        let peri = [...this.getSides()];
         let perifinal = 0;
-        for (let i = 0; i > peri.length; i++){
+        for (let i = 0; i < peri.length; i++) {
             perifinal += peri[i];
         }
         return perifinal;
     }
-    calcPeri2()
-    {
-        let perifinalfinal = square.height*2 + square.length*2;
+    calcPeri2() {
+        let perifinalfinal = this.height * 2 + this.width * 2;
         return perifinalfinal;
     }
 }
+
 const square = new Rectangle(10, 10);  
 console.log("Area: " + square.area); // 100
-console.log("Lados: "+ [...square.getSides()]); // [10, 10, 10, 10]
-console.log("Perimetro: "+ square.perimeter);
-console.log("Perimetro final: " + square.pera2);
+console.log("Lados: " + [...square.getSides()]); // [10, 10, 10, 10]
+console.log("Perimetro: " + square.perimeter);
+console.log("Perimetro final: " + square.pera2); 
